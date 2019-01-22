485d588116a123d05ac3b483f6a38da429a28e7c
Akira Hayakawa <ruby.wktk@gmail.com>
2019-01-22 20:44:09 +0900

    update
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/graph/apsp.rs
+++ src/graph/apsp.rs
@@ -1,10 +1,11 @@
 mod warshal_froid {
-    // dist could be negative
+    // dist could be negative as well as bellman ford
     fn warshal_froid(d: &mut [Vec<i32>]) {
         let n = d.len();
         for k in 0..n {
             for i in 0..n {
-                for j in 0..n { d[i][j] = std::cmp::min(d[i][j], d[i][k] + d[k][j]);
+                for j in 0..n {
+                     d[i][j] = std::cmp::min(d[i][j], d[i][k] + d[k][j]);
                 }
             }
         }
@@ -13,19 +14,20 @@ mod warshal_froid {
     fn test() {
         let dat = [
             (0,1,1),
-            (1,2,4),
             (0,3,2),
-            (3,4,2),
+            (0,4,3),
+            (1,2,4),
             (1,4,1),
             (2,4,1),
             (2,5,3),
-            (4,5,3)
+            (3,4,2),
+            (4,5,3),
         ];
 
         let INF = 1000000;
-        let mut dist = vec![vec![0; 6]; 6];
+        let mut dist = vec![vec![INF; 6]; 6];
         for i in 0 .. 6 {
-            dist[i][i] = INF;
+            dist[i][i] = 0;
         }
         for l in &dat {
             dist[l.0][l.1] = l.2;
@@ -33,5 +35,8 @@ mod warshal_froid {
         }
 
         warshal_froid(&mut dist);
+
+        assert_eq!(dist[0][5], 5);
+        assert_eq!(dist[2][3], 3);
     }
 }
