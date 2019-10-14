d2d3e8c76c5e4ba3e0414b687bd32b3594c6755c
Akira Hayakawa <ruby.wktk@gmail.com>
2019-10-14 10:25:19 +0900

    impl Imosu method
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/cumsum.rs
+++ src/cumsum.rs
@@ -61,9 +61,11 @@ impl CumSum2 {
             dp: vec![]
         }
     }
+    #[doc = "i~j"]
     fn add(&mut self, i: usize, j: usize, x: i64) {
         self.base[i][j] += x;
     }
+    #[doc = "i~j"]
     fn set(&mut self, i: usize, j: usize, x: i64) {
         self.base[i][j] = x;
     }
@@ -98,4 +100,73 @@ fn test_cum2() {
     assert_eq!(cum2.query(0, 1, 1, 2), 2);
     assert_eq!(cum2.query(1, 2, 1, 2), 4);
     assert_eq!(cum2.query(0, 1, 0, 2), 3);
+}
+
+#[snippet = "Imosu"]
+struct Imosu {
+    n: usize,
+    m: usize,
+    dp: Vec<Vec<i64>>,
+}
+#[snippet = "Imosu"]
+impl Imosu {
+    pub fn new(n: usize, m: usize) -> Imosu {
+        Imosu {
+            n: n,
+            m: m,
+            dp: vec![vec![0;m+1];n+1],
+        }
+    }
+    #[doc = "[i0,i1)~[j0,j1)"]
+    pub fn add(&mut self, i0: usize, i1: usize, j0: usize, j1: usize, x: i64) {
+        self.dp[i0][j0] = x;
+        self.dp[i0][j1] = -x;
+        self.dp[i1][j0] = -x;
+        self.dp[i1][j1] = x;
+    }
+    pub fn build(&mut self) {
+        // right sweep
+        for i in 0..self.n+1 {
+            for j in 0..self.m {
+                self.dp[i][j+1] += self.dp[i][j];
+            }
+        }
+        // down sweep
+        for j in 0..self.m+1 {
+            for i in 0..self.n {
+                self.dp[i+1][j] += self.dp[i][j];
+            }
+        }
+    }
+    pub fn get(&self, i: usize, j: usize) -> i64 {
+        self.dp[i][j]
+    }
+}
+#[test]
+fn test_imosu() {
+    let mut tbl = vec![vec![0;5];5];
+    let mut imosu = Imosu::new(5,5);
+
+    let tests = vec![
+        (0,3,0,3,5),
+        (2,4,1,3,8),
+        (1,3,2,4,3),
+    ];
+
+    for t in tests {
+        let (i0,i1,j0,j1,x) = t;
+        for i in i0..i1 {
+            for j in j0..j1 {
+                tbl[i][j] += x;
+            }
+        }
+        imosu.add(i0,i1,j0,j1,x);
+    }
+
+    imosu.build();
+    for i in 0..5 {
+        for j in 0..5 {
+            assert_eq!(imosu.get(i,j), tbl[i][j]);
+        }
+    }
 }
