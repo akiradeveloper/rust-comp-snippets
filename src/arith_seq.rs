d6e1c28a24a61d2d036d19e9dbcaac4912e5248e
Akira Hayakawa <ruby.wktk@gmail.com>
2019-12-18 21:33:23 +0900

    update
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/arith_seq.rs
+++ src/arith_seq.rs
@@ -7,9 +7,15 @@ struct ArithSeq {
 impl ArithSeq {
     #[doc = "a+bi >= x"]
     pub fn next(&self, x: i64) -> i64 {
-        let d = x - self.a;
-        let i = (d - 1 + self.b) / self.b;
-        self.a + self.b * i
+        if x >= self.a {
+            let d = x - self.a;
+            let i = (d - 1 + self.b) / self.b;
+            self.a + self.b * i
+        } else {
+            let d = self.a - x;
+            let i = d / self.b;
+            self.a - self.b * i
+        }
     }
     #[doc = "a+bi <= y"]
     pub fn prev(&self, x: i64) -> i64 {
@@ -21,8 +27,20 @@ impl ArithSeq {
         }
     }
     #[doc = "[a+bi, a+bj] <= [l, r]"]
-    pub fn range(&self, l: i64, u: i64) -> Option<(i64, i64, usize)> {
-        None
+    pub fn range(&self, l: i64, u: i64) -> Option<(i64, i64)> {
+        assert!(l<=u);
+        let x = self.next(l);
+        let y = self.prev(u);
+        if x > y {
+            return None
+        }
+        assert!(x <= y);
+        if l<=x && x<=u {
+            let cnt = (y-x) / self.b;
+            Some((x, cnt+1))
+        } else {
+            None
+        }
     }
 }
 
@@ -32,6 +50,12 @@ fn test_arith_seq_next() {
         a: 0,
         b: 3,
     };
+    assert_eq!(x.next(-6), -6);
+    assert_eq!(x.next(-5), -3);
+    assert_eq!(x.next(-4), -3);
+    assert_eq!(x.next(-3), -3);
+    assert_eq!(x.next(-2), 0);
+    assert_eq!(x.next(-1), 0);
     assert_eq!(x.next(0), 0);
     assert_eq!(x.next(1), 3);
     assert_eq!(x.next(2), 3);
@@ -42,6 +66,11 @@ fn test_arith_seq_next() {
         a: 1,
         b: 3,
     };
+    assert_eq!(x.next(-5), -5);
+    assert_eq!(x.next(-4), -2);
+    assert_eq!(x.next(-3), -2);
+    assert_eq!(x.next(-2), -2);
+    assert_eq!(x.next(-1), 1);
     assert_eq!(x.next(0), 1);
     assert_eq!(x.next(1), 1);
     assert_eq!(x.next(2), 4);
@@ -56,9 +85,31 @@ fn test_arith_seq_prev() {
         a: 1,
         b: 3,
     };
+    assert_eq!(x.prev(6), 4);
+    assert_eq!(x.prev(5), 4);
     assert_eq!(x.prev(4), 4);
     assert_eq!(x.prev(3), 1);
     assert_eq!(x.prev(2), 1);
     assert_eq!(x.prev(1), 1);
     assert_eq!(x.prev(0), -2);
+    assert_eq!(x.prev(-1), -2);
+    assert_eq!(x.prev(-2), -2);
+    assert_eq!(x.prev(-3), -5);
+    assert_eq!(x.prev(-4), -5);
+    assert_eq!(x.prev(-5), -5);
+}
+
+#[test]
+fn test_arith_seq_range() {
+    let x = ArithSeq {
+        a: 1,
+        b: 3,
+    };
+    assert_eq!(x.range(1,4), Some((1,2)));
+    assert_eq!(x.range(1,3), Some((1,1)));
+    assert_eq!(x.range(2,3), None);
+    assert_eq!(x.range(2,4), Some((4,1)));
+    assert_eq!(x.range(0,5), Some((1,2)));
+    assert_eq!(x.range(0,4), Some((1,2)));
+    assert_eq!(x.range(-3,5), Some((-2,3)));
 }
