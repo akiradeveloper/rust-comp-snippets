85b3dd2d718ff9c428b8c7937113c76665a42886
Akira Hayakawa <ruby.wktk@gmail.com>
2020-02-09 20:53:33 +0900

    update
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/boundary.rs
+++ src/boundary.rs
@@ -15,3 +15,14 @@ pub fn adj4(i: usize, j: usize, h: usize, w: usize) -> Vec<(usize, usize)> {
     }
     r
 }
+
+#[snippet = "valid4"]
+pub fn valid4(h: usize, w: usize, ps: Vec<(i64,i64)>) -> Vec<(usize,usize)> {
+    let mut res = vec![];
+    for (i,j) in ps {
+        if 0<=i && i<h as i64 && 0<=j && j<w as i64 {
+            res.push((i as usize, j as usize))
+        }
+    }
+    res
+}
