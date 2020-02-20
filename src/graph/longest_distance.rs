f45d377043c2168adfc0a342c4eddac7bf7ee28d
Akira Hayakawa <ruby.wktk@gmail.com>
2020-02-20 19:34:52 +0900

    update
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/graph/longest_distance.rs
+++ src/graph/longest_distance.rs
@@ -6,14 +6,16 @@ struct LongestDistance {
 #[snippet = "LongestDistance"]
 impl LongestDistance {
     #[doc = "DAG"]
-    fn new(ing: Vec<Vec<usize>>) -> LongestDistance {
-        let n = ing.len();
+    pub fn new(n: usize) -> LongestDistance {
         LongestDistance {
-            ing: ing,
+            ing: vec![vec![]; n],
             dp: vec![None; n],
         }
     }
-    fn solve(&mut self, i: usize) -> usize {
+    pub fn add_edge(&mut self, u: usize, v: usize) {
+        self.ing[v].push(u)
+    }
+    pub fn solve(&mut self, i: usize) -> usize {
         if self.ing[i].len() == 0 {
             return 0
         }
@@ -28,4 +30,18 @@ impl LongestDistance {
         self.dp[i] = Some(maxv);
         return maxv;
     }
+}
+#[test]
+fn test_longest_distance() {
+    let mut g = LongestDistance::new(8);
+    let e = vec![
+        (0,1),(2,0),(1,3),(1,4),(5,2),(4,6),(4,7),
+    ];
+    for (u,v) in e {
+        g.add_edge(u, v);
+    }
+    assert_eq!(g.solve(7), 5);
+    assert_eq!(g.solve(2), 1);
+    assert_eq!(g.solve(5), 0);
+    assert_eq!(g.solve(3), 4);
 }
