a09483261204b37b13936db325f2a79ca972602e
Akira Hayakawa <ruby.wktk@gmail.com>
2020-02-29 19:26:10 +0900

    update
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/lcs.rs
+++ src/lcs.rs
@@ -9,25 +9,26 @@ impl <T: Eq> LCS<T> {
         let n = xs.len();
         let m = ys.len();
         let dp = Self::lcs(xs, ys);
-        let k = dp[n-1][m-1];
-        let mut matching = vec![(0,0);k];
-        let mut cur = 0;
-        for i in 0..xs.len() {
-            for j in 0..ys.len() {
-                if dp[i][j] > cur {
-                    matching[cur] = (i,j);
-                    cur += 1;
-                }
+        let mut k = dp[n][m];
+        let mut i = n;
+        let mut j = m;
+        let mut matching = vec![];
+        while k>0 {
+            while dp[i-1][j] == k {
+                i -= 1;
             }
-        }
-        let mut tmp = vec![vec![0;m+1];n+1];
-        for i in 0..n {
-            for j in 0..m {
-                tmp[i+1][j+1] = dp[i][j];
+            while dp[i][j-1] == k {
+                j -= 1;
             }
+            matching.push((i-1,j-1));
+            k -= 1;
+            i -= 1;
+            j -= 1;
         }
+        matching.sort();
+        
         LCS {
-            max_matching: tmp,
+            max_matching: dp,
             matching: matching,
             phantom: std::marker::PhantomData,
         }
@@ -35,41 +36,18 @@ impl <T: Eq> LCS<T> {
     fn lcs(xs: &[T], ys: &[T]) -> Vec<Vec<usize>> {
         let n = xs.len();
         let m = ys.len();
-        let mut dp = vec![vec![0; m]; n];
-        for i in 0..n {
-            let x = &xs[i];
-            let mut found = false;
-            for j in 0..m {
-                if &ys[j] == x {
-                    found = true;
-                }
-                dp[i][j] = if found {
-                    1
-                } else { 0 };
-            }
-        }
-        for j in 0..m {
-            let y = &ys[j];
-            let mut found = false;
-            for i in 0..n {
-                if &xs[i] == y {
-                    found = true;
-                }
-                dp[i][j] = if found {
-                    1
-                } else { 0 };
-            }
-        }
-        for i in 1..n {
-            for j in 1..m {
-                dp[i][j] = if xs[i] == ys[j] {
+        let mut dp = vec![vec![0;m+1];n+1];
+        dp[0][0] = 0;
+        for i in 1..=n {
+            for j in 1..=m {
+                dp[i][j] = if xs[i-1] == ys[j-1] {
                     dp[i-1][j-1] + 1
                 } else {
                     std::cmp::max(dp[i][j-1], dp[i-1][j])
-                };
+                }
             }
         }
-        return dp;
+        dp
     }
 }
 
@@ -86,4 +64,5 @@ fn test_lcs1() {
     let ys = vec![1,0,0,1,1,1,1,1,0];
     let lcs = LCS::new(&xs, &ys);
     assert_eq!(lcs.matching.len(), 7);
+    assert_eq!(lcs.matching, vec![(0,0),(1,1),(2,3),(3,4),(4,5),(5,6),(6,8)]);
 }
