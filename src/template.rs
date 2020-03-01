a2fa70d3ef8ea990c0c01f1b14c4b48ac43b7326
Akira Hayakawa <ruby.wktk@gmail.com>
2020-03-01 21:18:27 +0900

    add cfor
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/template.rs
+++ src/template.rs
@@ -54,6 +54,59 @@ macro_rules! dvec {
     };
 }
 
+#[macro_export]
+macro_rules! cfor {
+    // for (; ...; ...) { ... }
+    (; $($rest: tt)*) => {
+        cfor!((); $($rest)*)
+    };
+    // for ($init; ; ...) { ... }
+    ($($init: stmt),+; ; $($rest: tt)*) => {
+        // avoid the `while true` lint
+        cfor!($($init),+; !false; $($rest)*)
+    };
+
+    // for ($init; $cond; ) { ... }
+    ($($init: stmt),+; $cond: expr; ; $body: block) => {
+        cfor!{$($init),+; $cond; (); $body}
+    };
+
+    // for ($init; $cond; $step) { $body }
+    ($($init: stmt),+; $cond: expr; $($step: expr),+; $body: block) => {
+        {
+            $($init;)+
+            while $cond {
+                let mut _first = true;
+                let mut _continue = false;
+                // this loop runs once, allowing us to use `break` and
+                // `continue` as `goto` to skip forward to the
+                // condition.
+                //
+                // the booleans above are very transparent to the
+                // optimiser, since they are modified exactly once,
+                // with nice control flow, and this this optimises to
+                // be similar to C for loop.
+                loop {
+                    // if we *don't* hit this, there was a `break` in
+                    // the body (otherwise the loop fell-through or
+                    // was `continue`d.)
+                    if !_first { _continue = true; break }
+                    _first = false;
+
+                    $body
+                }
+                if !_continue {
+                    // the `if` wasn't hit, so we should propagate the
+                    // `break`.
+                    break
+                }
+
+                $($step;)+
+            }
+        }
+    };
+}
+
 /// main
 
 #[allow(unused_imports)]
