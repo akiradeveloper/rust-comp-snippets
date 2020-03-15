fea2d072cde6697b2970a6131e3f65f519ae25ad
Akira Hayakawa <ruby.wktk@gmail.com>
2020-03-15 10:58:01 +0900

    update
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/boundary.rs
+++ src/boundary.rs
@@ -1,15 +1,4 @@
 #[snippet = "valid4"]
-pub fn valid4(h: usize, w: usize, ps: Vec<(i64,i64)>) -> Vec<(usize,usize)> {
-    let mut res = vec![];
-    for (i,j) in ps {
-        if 0<=i && i<h as i64 && 0<=j && j<w as i64 {
-            res.push((i as usize, j as usize))
-        }
-    }
-    res
-}
-
-#[snippet = "valid4u"]
 pub fn valid4u(h: usize, w: usize, ps: Vec<(Option<usize>, Option<usize>)>) -> Vec<(usize, usize)> {
     let mut res = vec![];
     for (x, y) in ps {
