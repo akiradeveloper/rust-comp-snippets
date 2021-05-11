0f16bb1aeb026727d2ad2818f8498f87d7866065
Akira Hayakawa <ruby.wktk@gmail.com>
2021-05-11 15:43:57 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/arith_seq.rs
+++ src/arith_seq.rs
@@ -14,6 +14,7 @@ impl ArithSeq {
             b: b,
         }
     }
+    /// (x0,y0)を通る傾きaの直線を求める
     pub fn from_point(x0: i64, y0: i64, a: i64) -> Self {
         let b = y0 - a * x0;
         Self::new(a,b)
@@ -21,8 +22,8 @@ impl ArithSeq {
     pub fn y(&self, x: i64) -> i64 {
         self.a * x + self.b
     }
-    #[doc = "f(x-1) < y かつ f(x) >= y となるようなx"]
-    pub fn lower_bound(&self, y: i64) -> i64 {
+    /// ceil(水平線y=y0との交点)
+    pub fn x(&self, y: i64) -> i64 {
         assert!(self.a != 0);
         let a = self.a;
         let b = self.b;
@@ -35,33 +36,31 @@ impl ArithSeq {
             let x = x / a;
             x
         } else {
-            let y = 2*b - y;
-            let mirror = Self::new(-a,b);
-            mirror.lower_bound(y)
+            // (b+c)/2=y0
+            let c = 2*y - b;
+            // y方向にミラーする
+            let mirror = Self::new(-a,c);
+            mirror.x(y)
         }
-    }
-    #[doc = "f(x) < y かつ f(x+1) >= y となるようなx"]
-    pub fn upper_bound(&self, y: i64) -> i64 {
-        unimplemented!()
     }
 }
 #[test]
 fn test_arith_seq_lower_bound() {
     let f = ArithSeq::new(3,1);
-    assert_eq!(f.lower_bound(-2), -1);
-    assert_eq!(f.lower_bound(1), 0);
-    assert_eq!(f.lower_bound(2), 1);
-    assert_eq!(f.lower_bound(3), 1);
-    assert_eq!(f.lower_bound(4), 1);
-    assert_eq!(f.lower_bound(5), 2);
+    assert_eq!(f.x(-2), -1);
+    assert_eq!(f.x(1), 0);
+    assert_eq!(f.x(2), 1);
+    assert_eq!(f.x(3), 1);
+    assert_eq!(f.x(4), 1);
+    assert_eq!(f.x(5), 2);
 
     let f = ArithSeq::new(-3,1);
-    assert_eq!(f.lower_bound(4), -1);
-    assert_eq!(f.lower_bound(3), 0);
-    assert_eq!(f.lower_bound(2), 0);
-    assert_eq!(f.lower_bound(1), 0);
-    assert_eq!(f.lower_bound(0), 1);
-    assert_eq!(f.lower_bound(-1), 1);
-    assert_eq!(f.lower_bound(-2), 1);
-    assert_eq!(f.lower_bound(-3), 2);
+    assert_eq!(f.x(4), -1);
+    assert_eq!(f.x(3), 0);
+    assert_eq!(f.x(2), 0);
+    assert_eq!(f.x(1), 0);
+    assert_eq!(f.x(0), 1);
+    assert_eq!(f.x(-1), 1);
+    assert_eq!(f.x(-2), 1);
+    assert_eq!(f.x(-3), 2);
 }
