0139a53adff2f4eb3dc8afcfe0d2a8976682a645
Akira Hayakawa <ruby.wktk@gmail.com>
2021-05-11 18:17:46 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/arith_seq.rs
+++ src/arith_seq.rs
@@ -19,11 +19,11 @@ impl ArithSeq {
         let b = y0 - a * x0;
         Self::new(a,b)
     }
-    pub fn y(&self, x: i64) -> i64 {
-        self.a * x + self.b
+    pub fn y(&self, x0: i64) -> i64 {
+        self.a * x0 + self.b
     }
-    /// ceil(水平線y=y0との交点)
-    pub fn x(&self, y: i64) -> i64 {
+    /// ceil(y=y0との交点)
+    pub fn x_ceil(&self, y: i64) -> i64 {
         assert!(self.a != 0);
         let a = self.a;
         let b = self.b;
@@ -40,27 +40,42 @@ impl ArithSeq {
             let c = 2*y - b;
             // y方向にミラーする
             let mirror = Self::new(-a,c);
-            mirror.x(y)
+            mirror.x_ceil(y)
+        }
+    }
+    /// floor(y=y0との交点)
+    pub fn x_floor(&self, y0: i64) -> i64 {
+        let x0 = self.x_ceil(y0);
+        let y = self.y(x0);
+        if y == y0 {
+            return x0;
+        }
+        if self.a > 0 && y > y0 {
+            x0 - 1
+        } else if self.a < 0 && y < y0 {
+            x0 - 1
+        } else {
+            x0
         }
     }
 }
 #[test]
 fn test_arith_seq_lower_bound() {
     let f = ArithSeq::new(3,1);
-    assert_eq!(f.x(-2), -1);
-    assert_eq!(f.x(1), 0);
-    assert_eq!(f.x(2), 1);
-    assert_eq!(f.x(3), 1);
-    assert_eq!(f.x(4), 1);
-    assert_eq!(f.x(5), 2);
+    assert_eq!(f.x_ceil(-2), -1);
+    assert_eq!(f.x_ceil(1), 0);
+    assert_eq!(f.x_ceil(2), 1);
+    assert_eq!(f.x_ceil(3), 1);
+    assert_eq!(f.x_ceil(4), 1);
+    assert_eq!(f.x_ceil(5), 2);
 
     let f = ArithSeq::new(-3,1);
-    assert_eq!(f.x(4), -1);
-    assert_eq!(f.x(3), 0);
-    assert_eq!(f.x(2), 0);
-    assert_eq!(f.x(1), 0);
-    assert_eq!(f.x(0), 1);
-    assert_eq!(f.x(-1), 1);
-    assert_eq!(f.x(-2), 1);
-    assert_eq!(f.x(-3), 2);
+    assert_eq!(f.x_ceil(4), -1);
+    assert_eq!(f.x_ceil(3), 0);
+    assert_eq!(f.x_ceil(2), 0);
+    assert_eq!(f.x_ceil(1), 0);
+    assert_eq!(f.x_ceil(0), 1);
+    assert_eq!(f.x_ceil(-1), 1);
+    assert_eq!(f.x_ceil(-2), 1);
+    assert_eq!(f.x_ceil(-3), 2);
 }
