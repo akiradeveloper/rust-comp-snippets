757d109f94c8b1a3fefb20de847aac56e09de399
Akira Hayakawa <ruby.wktk@gmail.com>
2021-05-16 20:48:54 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/seg_lazy.rs
+++ src/seg_lazy.rs
@@ -5,19 +5,22 @@ use cargo_snippet::snippet;
 #[snippet("SEG_LAZY")]
 trait SEGLazyImpl {
     type Monoid: Copy;
-    type OperatorMonoid: Copy + PartialEq;
-    fn m0() -> Self::Monoid;
-    fn om0() -> Self::OperatorMonoid;
-    fn f(x: Self::Monoid, y: Self::Monoid) -> Self::Monoid;
-    fn g(x: Self::Monoid, y: Self::OperatorMonoid) -> Self::Monoid;
-    fn h(x: Self::OperatorMonoid, y: Self::OperatorMonoid) -> Self::OperatorMonoid;
+    type F: Copy + PartialEq;
+    fn e() -> Self::Monoid;
+    fn id() -> Self::F;
+    /// x `op` y
+    fn op(x: Self::Monoid, y: Self::Monoid) -> Self::Monoid;
+    /// f(x)
+    fn mapping(f: Self::F, x: Self::Monoid) -> Self::Monoid;
+    /// g . f
+    fn compose(f: Self::F, g: Self::F) -> Self::F;
 }
 
 #[snippet("SEG_LAZY")]
 struct SEGLazy<T: SEGLazyImpl> {
     n: usize,
     data: Vec<T::Monoid>,
-    lazy: Vec<T::OperatorMonoid>,
+    lazy: Vec<T::F>,
 }
 
 #[snippet("SEG_LAZY")]
@@ -28,29 +31,29 @@ impl <T: SEGLazyImpl> SEGLazy<T> {
         SEGLazy {
             n: m,
             data: vec![init; m*2],
-            lazy: vec![T::om0(); m*2],
+            lazy: vec![T::id(); m*2],
         }
     }
     fn propagate(&mut self, k: usize) {
-        if self.lazy[k] != T::om0() {
+        if self.lazy[k] != T::id() {
             if k < self.n {
-                self.lazy[2*k+0] = T::h(self.lazy[2*k+0], self.lazy[k]);
-                self.lazy[2*k+1] = T::h(self.lazy[2*k+1], self.lazy[k]);
+                self.lazy[2*k+0] = T::compose(self.lazy[2*k+0], self.lazy[k]);
+                self.lazy[2*k+1] = T::compose(self.lazy[2*k+1], self.lazy[k]);
             }
-            self.data[k] = T::g(self.data[k], self.lazy[k]);
-            self.lazy[k] = T::om0();
+            self.data[k] = T::mapping(self.lazy[k], self.data[k]);
+            self.lazy[k] = T::id();
         }
     }
-    fn do_update(&mut self, a: usize, b: usize, x: T::OperatorMonoid, k: usize, l: usize, r: usize) -> T::Monoid {
+    fn do_update(&mut self, a: usize, b: usize, x: T::F, k: usize, l: usize, r: usize) -> T::Monoid {
         self.propagate(k);
         if r <= a || b <= l {
             self.data[k]
         } else if a <= l && r <= b {
-            self.lazy[k] = T::h(self.lazy[k], x);
+            self.lazy[k] = T::compose(self.lazy[k], x);
             self.propagate(k);
             self.data[k]
         } else {
-            self.data[k] = T::f(
+            self.data[k] = T::op(
                 self.do_update(a, b, x, 2*k+0, l, (l+r)>>1),
                 self.do_update(a, b, x, 2*k+1, (l+r)>>1, r)
             );
@@ -58,18 +61,18 @@ impl <T: SEGLazyImpl> SEGLazy<T> {
         }
     }
     #[doc = "[l,r)"]
-    pub fn update(&mut self, l: usize, r: usize, x: T::OperatorMonoid) -> T::Monoid {
+    pub fn update(&mut self, l: usize, r: usize, x: T::F) -> T::Monoid {
         let n = self.n;
         self.do_update(l, r, x, 1, 0, n)
     }
     fn do_query(&mut self, a: usize, b: usize, k: usize, l: usize, r: usize) -> T::Monoid {
         self.propagate(k);
         if r <= a || b <= l {
-            T::m0()
+            T::e()
         } else if a <= l && r <= b {
             self.data[k]
         } else {
-            T::f(
+            T::op(
                 self.do_query(a, b, 2*k+0, l, (l+r)>>1),
                 self.do_query(a, b, 2*k+1, (l+r)>>1, r)
             )
@@ -87,26 +90,26 @@ struct MAX_RUQ;
 #[snippet("SEG_LAZY_MAX_RUQ")]
 impl SEGLazyImpl for MAX_RUQ {
     type Monoid = i64;
-    type OperatorMonoid = i64;
-    fn m0() -> Self::Monoid {
+    type F = i64;
+    fn e() -> Self::Monoid {
         0
     }
-    fn om0() -> Self::OperatorMonoid {
+    fn id() -> Self::F {
         0
     }
-    fn f(x: Self::Monoid, y: Self::Monoid) -> Self::Monoid {
+    fn op(x: Self::Monoid, y: Self::Monoid) -> Self::Monoid {
         std::cmp::max(x, y)
     }
-    fn g(x: Self::Monoid, y: Self::OperatorMonoid) -> Self::Monoid {
-        y
+    fn mapping(f: Self::F, x: Self::Monoid) -> Self::Monoid {
+        f
     }
-    fn h(x: Self::OperatorMonoid, y: Self::OperatorMonoid) -> Self::OperatorMonoid {
-        y
+    fn compose(f: Self::F, g: Self::F) -> Self::F {
+        g
     }
 }
 #[test]
 fn test_MAX_RUQ() {
-    let mut seg: SEGLazy<MAX_RUQ> = SEGLazy::new(10, MAX_RUQ::m0());
+    let mut seg: SEGLazy<MAX_RUQ> = SEGLazy::new(10, MAX_RUQ::id());
     assert_eq!(seg.query(0, 3), 0);
     seg.update(0, 2, 10); // [10,10,0,...]
     assert_eq!(seg.query(0, 3), 10);
@@ -123,26 +126,26 @@ struct MIN_RUQ;
 #[snippet("SEG_LAZY_MIN_RUQ")]
 impl SEGLazyImpl for MIN_RUQ {
     type Monoid = i64;
-    type OperatorMonoid = i64;
-    fn m0() -> Self::Monoid {
+    type F = i64;
+    fn e() -> Self::Monoid {
         std::i64::MAX
     }
-    fn om0() -> Self::OperatorMonoid {
+    fn id() -> Self::F {
         std::i64::MAX
     }
-    fn f(x: Self::Monoid, y: Self::Monoid) -> Self::Monoid {
+    fn op(x: Self::Monoid, y: Self::Monoid) -> Self::Monoid {
         std::cmp::min(x, y)
     }
-    fn g(x: Self::Monoid, y: Self::OperatorMonoid) -> Self::Monoid {
-        y
+    fn mapping(f: Self::F, x: Self::Monoid) -> Self::Monoid {
+        f
     }
-    fn h(x: Self::OperatorMonoid, y: Self::OperatorMonoid) -> Self::OperatorMonoid {
-        y
+    fn compose(f: Self::F, g: Self::F) -> Self::F {
+        g
     }
 }
 #[test]
 fn test_MIN_RUQ() { // DSL_2_D
-    let mut seg: SEGLazy<MIN_RUQ> = SEGLazy::new(8, MIN_RUQ::m0());
+    let mut seg: SEGLazy<MIN_RUQ> = SEGLazy::new(8, MIN_RUQ::e());
     seg.update(1,7,5);
     seg.update(2,8,2);
     seg.update(2,6,7);
@@ -160,21 +163,21 @@ struct MAX_RAQ;
 #[snippet("SEG_LAZY_MAX_RAQ")]
 impl SEGLazyImpl for MAX_RAQ {
     type Monoid = i64;
-    type OperatorMonoid = i64;
-    fn m0() -> Self::Monoid {
+    type F = i64;
+    fn e() -> Self::Monoid {
         std::i64::MIN
     }
-    fn om0() -> Self::OperatorMonoid {
+    fn id() -> Self::F {
         0
     }
-    fn f(x: Self::Monoid, y: Self::Monoid) -> Self::Monoid {
+    fn op(x: Self::Monoid, y: Self::Monoid) -> Self::Monoid {
         std::cmp::max(x, y)
     }
-    fn g(x: Self::Monoid, y: Self::OperatorMonoid) -> Self::Monoid {
-        x + y
+    fn mapping(f: Self::F, x: Self::Monoid) -> Self::Monoid {
+        x + f
     }
-    fn h(x: Self::OperatorMonoid, y: Self::OperatorMonoid) -> Self::OperatorMonoid {
-        x + y
+    fn compose(f: Self::F, g: Self::F) -> Self::F {
+        f + g
     }
 }
 
@@ -183,21 +186,21 @@ struct MIN_RAQ;
 #[snippet("SEG_LAZY_MIN_RAQ")]
 impl SEGLazyImpl for MIN_RAQ { 
     type Monoid = i64;
-    type OperatorMonoid = i64;
-    fn m0() -> Self::Monoid {
+    type F = i64;
+    fn e() -> Self::Monoid {
         std::i64::MAX
     }
-    fn om0() -> Self::OperatorMonoid {
+    fn id() -> Self::F {
         0
     }
-    fn f(x: Self::Monoid, y: Self::Monoid) -> Self::Monoid {
+    fn op(x: Self::Monoid, y: Self::Monoid) -> Self::Monoid {
         std::cmp::min(x, y)
     }
-    fn g(x: Self::Monoid, y: Self::OperatorMonoid) -> Self::Monoid {
-        x + y
+    fn mapping(f: Self::F, x: Self::Monoid) -> Self::Monoid {
+        x + f
     }
-    fn h(x: Self::OperatorMonoid, y: Self::OperatorMonoid) -> Self::OperatorMonoid {
-        x + y
+    fn compose(f: Self::F, g: Self::F) -> Self::F {
+        f + g
     }
 }
 #[test]
