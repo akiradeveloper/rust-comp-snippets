72e5293108da5c39e1d7566e3d4e64ab74b6c91e
Akira Hayakawa <ruby.wktk@gmail.com>
2021-05-20 16:49:27 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/bitop.rs
+++ src/bitop.rs
@@ -1,5 +1,9 @@
 use cargo_snippet::snippet;
 
+/// ある値に含まれるmビットを
+/// 反転させた値2^m通りを計算する。
+/// 計算量: O(m)
+
 #[snippet("submasks")]
 fn submasks(mask: i64) -> Vec<i64> {
     let mut res = vec![];
@@ -8,16 +12,22 @@ fn submasks(mask: i64) -> Vec<i64> {
         res.push(smask);
         smask = (smask-1) & mask;
     }
+    res.push(0);
     res.reverse();
     return res
 }
 #[test]
 fn test_submasks() {
-    assert_eq!(submasks(13), [1,4,5,8,9,12,13]);
+    // 13 = 1101
+    assert_eq!(submasks(13), [0,1,4,5,8,9,12,13]);
 }
 
-#[snippet("bitpos")]
-fn bitpos(x: i64) -> Vec<usize> {
+/// bitが立っている箇所を計算する
+/// 計算量:
+/// 構築 O(log x)
+
+#[snippet("bit_positions")]
+fn bit_positions(x: i64) -> Vec<usize> {
     let mut p = vec![];
     let mut cur = x;
     for i in 0.. {
@@ -30,18 +40,21 @@ fn bitpos(x: i64) -> Vec<usize> {
     p
 }
 #[test]
-fn test_bitpos() {
-    assert_eq!(bitpos(0b11000001), [0,6,7]);
+fn test_bit_positions() {
+    assert_eq!(bit_positions(0b11000001), [0,6,7]);
 }
 
-#[snippet("BitMask")]
-struct BitMask {
+/// bit演算を行うクラス
+/// 計算量: 全部O(1)
+
+#[snippet("BitOp")]
+struct BitOp {
     x: i64,
 }
-#[snippet("BitMask")]
-impl BitMask {
+#[snippet("BitOp")]
+impl BitOp {
     pub fn new(x: i64) -> Self {
-        BitMask { x: x }
+        Self { x: x }
     }
     pub fn check(&self, k: usize) -> bool {
         self.x & (1<<k) > 0
@@ -56,7 +69,8 @@ impl BitMask {
     pub fn flip(&self, k: usize) -> i64 {
         self.x ^ (1<<k)
     }
-    #[doc = "0b1110 -> 0b10"]
+    /// もっとも右に立ってるビットのみを返す。
+    /// 例: 0x1010 -> 0x10
     pub fn lsb(&self) -> i64 {
         let x = self.x;
         x & -x
@@ -65,7 +79,7 @@ impl BitMask {
 
 #[test]
 fn test_bitmask() {
-    let x = BitMask::new(0b1101);
+    let x = BitOp::new(0b1101);
     assert_eq!(x.flip(2), 0b1001);
     assert_eq!(x.flip(1), 0b1111);
     assert_eq!(x.off(0), 0b1100);
@@ -76,8 +90,8 @@ fn test_bitmask() {
 
 #[test]
 fn test_lsb() {
-    assert_eq!(BitMask::new(3).lsb(), 1);
-    assert_eq!(BitMask::new(5).lsb(), 1);
-    assert_eq!(BitMask::new(6).lsb(), 2);
-    assert_eq!(BitMask::new(4).lsb(), 4);
+    assert_eq!(BitOp::new(3).lsb(), 1);
+    assert_eq!(BitOp::new(5).lsb(), 1);
+    assert_eq!(BitOp::new(6).lsb(), 2);
+    assert_eq!(BitOp::new(4).lsb(), 4);
 }
