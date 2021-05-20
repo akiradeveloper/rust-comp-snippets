5f5c39af521e37a39b5a96eabed0544679c5197c
Akira Hayakawa <ruby.wktk@gmail.com>
2021-05-20 12:53:09 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/sqrt_decomposition.rs
+++ src/sqrt_decomposition.rs
@@ -4,47 +4,61 @@ use cargo_snippet::snippet;
 #[derive(Debug, Copy, Clone)]
 enum Bucket {
     #[doc = "b-idx"]
-    Filled(usize),
+    Filled { bucket_index: usize },
     #[doc = "b-idx, [to, from)"]
-    Partial(usize, usize, usize),
+    Partial { bucket_index: usize, l: usize, r: usize },
 }
 #[snippet("SqrtDecomposition")]
 struct Buckets {
-    n: usize,
-    d: usize,
+    pub d: usize,
 }
 #[snippet("SqrtDecomposition")]
 impl Buckets {
+    pub fn new(n: usize) -> Self {
+        let mut d = 1;
+        while d*d < n {
+            d += 1;
+        }
+        Self {
+            d,
+        }
+    }
+    pub fn n(&self) -> usize {
+        self.d * self.d
+    }
     pub fn bucket(&self, i: usize) -> usize {
         i / self.d
     }
-    
-    #[doc = "[from, to)"]
-    pub fn buckets(&self, from: usize, to: usize) -> Vec<Bucket> {
+    #[doc = "[l, r)"]
+    pub fn buckets(&self, l: usize, r: usize) -> Vec<Bucket> {
+        if l == r {
+            return vec![]
+        }
+        assert!(r>l);
+
         let mut res = vec![];
-        let mut i = 0;
-        while to - i > 0 {
-            let b = i / self.d;
-            let next_boundary = (b+1) * self.d;
-            if i % self.d == 0 {
-                if next_boundary <= to {
-                    // fill
-                    res.push(Bucket::Filled(b));
-                    i = next_boundary;
-                } else {
-                    // partial
-                    res.push(Bucket::Partial(b, i, to));
-                    i = to;
-                }
+        let bl = l / self.d;
+        let br = (r-1) / self.d;
+        if bl == br {
+            if l % self.d == 0 && r % self.d == 0 {
+                res.push(Bucket::Filled { bucket_index: bl });
             } else {
-                // partial
-                if next_boundary <= to {
-                    res.push(Bucket::Partial(b, i, next_boundary));
-                    i = next_boundary;
-                } else {
-                    res.push(Bucket::Partial(b, i, to));
-                    i = to
-                }
+                res.push(Bucket::Partial { bucket_index: bl, l, r });
+            }
+        } else {
+            if l % self.d == 0 {
+                res.push(Bucket::Filled { bucket_index: bl });
+            } else {
+                let bnext = bl+1;
+                res.push(Bucket::Partial { bucket_index: bl, l, r: bnext * self.d });
+            }
+            for i in bl+1..br {
+                res.push(Bucket::Filled { bucket_index: i });
+            }
+            if r % self.d == 0 {
+                res.push(Bucket::Filled { bucket_index: br });
+            } else {
+                res.push(Bucket::Partial { bucket_index: br, l: br * self.d, r });
             }
         }
         res
@@ -52,16 +66,15 @@ impl Buckets {
 }
 #[test]
 fn test_buckets() {
-    let sc = Buckets {
-        n: 10,
-        d: 3,
-    };
-    assert_eq!(sc.bucket((0)), 0);
-    assert_eq!(sc.bucket((4)), 1);
+    let sc = Buckets::new(10);
+    assert_eq!(sc.d, 4);
+    assert_eq!(sc.bucket(0), 0);
+    assert_eq!(sc.bucket(4), 1);
     dbg!(sc.buckets(0, 3));
     dbg!(sc.buckets(0, 4));
     dbg!(sc.buckets(1, 4));
     dbg!(sc.buckets(2, 6));
     dbg!(sc.buckets(2, 9));
     dbg!(sc.buckets(2, 10));
+    dbg!(sc.buckets(2, 12));
 }
