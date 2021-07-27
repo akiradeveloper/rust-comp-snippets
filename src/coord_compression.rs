afd724f65943172b07919a87d86f617c1b15a0ed
Akira Hayakawa <ruby.wktk@gmail.com>
2021-07-27 21:19:23 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/coord_compression.rs
+++ src/coord_compression.rs
@@ -1,13 +1,13 @@
 use cargo_snippet::snippet;
 
 #[snippet("CoordCompression")]
-struct CoordCompression {
+pub struct CoordCompression {
     comp: std::collections::HashMap<i64,usize>,
     dcmp: std::collections::HashMap<usize,i64>,
 }
 #[snippet("CoordCompression")]
 impl CoordCompression {
-    pub fn new(xs: &[i64], start: usize) -> CoordCompression {
+    pub fn new(xs: &[i64], start: usize, step: usize) -> CoordCompression {
         let mut xs = xs.to_owned();
         xs.sort();
         let mut comp = std::collections::HashMap::new();
@@ -17,7 +17,7 @@ impl CoordCompression {
             if comp.contains_key(&x) { continue; }
             comp.insert(x,acc);
             dcmp.insert(acc,x);
-            acc+=1;
+            acc += step;
         }
         CoordCompression {
             comp: comp,
@@ -30,11 +30,14 @@ impl CoordCompression {
     pub fn decompress(&self, x: usize) -> i64 {
         *self.dcmp.get(&x).unwrap()
     }
+    pub fn n(&self) -> usize {
+        self.comp.len()
+    }
 }
 #[test]
 fn test_coord_compression() {
     let v = vec![-2,3,99999,1000];
-    let cc = CoordCompression::new(&v,0);
+    let cc = CoordCompression::new(&v,0,1);
     assert_eq!(cc.compress(-2),0);
     assert_eq!(cc.compress(1000),2);
     assert_eq!(cc.decompress(1), 3);
