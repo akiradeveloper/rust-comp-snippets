6011684fd8edcfac56ea6235f9e87666659ef4fc
Akira Hayakawa <ruby.wktk@gmail.com>
2021-08-18 13:59:40 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/permutation.rs
+++ src/permutation.rs
@@ -1,13 +1,50 @@
 use cargo_snippet::snippet;
 
-/// 置換の二行記法
-/// 
-/// サイクルに分割することが出来る。
-/// 
-/// 計算量 O(N logN)
+/// 互換 (x,y)
+#[derive(PartialEq, Debug, Clone, Copy)]
+pub struct Transposition {
+    pub x: usize,
+    pub y: usize,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+/// 巡回置換
+pub struct CyclicPermutation {
+    member: Vec<usize>,
+}
+impl CyclicPermutation {
+    fn new(xs: Vec<usize>) -> Self {
+        CyclicPermutation { member: xs }
+    }
+    /// 巡回置換(a0,a1,a2...,an)は、
+    /// 互換の列(a0,an),...,(a0,a2),(a0,a1)
+    /// と等価である。
+    /// 
+    /// ある置換を構成する互換の個数が
+    /// 偶数個の時、偶置換
+    /// 奇数個の時、奇置換
+    /// という。
+    pub fn into_transpositions(self) -> Vec<Transposition> {
+        if self.member.len() == 0 {
+            vec![]
+        } else {
+            let mut ret = vec![];
+            let x = self.member[0];
+            let n = self.member.len();
+            for i in 1..n {
+                ret.push(Transposition { x, y: self.member[i] });
+            }
+            ret.reverse();
+            ret
+        }
+    }
+}
+
 
 #[snippet("Permutation")]
-struct Permutation {
+/// 置換の二行記法
+#[derive(Debug, Clone)]
+pub struct Permutation {
     a: Vec<usize>,
     b: Vec<usize>
 }
@@ -20,7 +57,9 @@ impl Permutation {
             b: b,
         }
     }
-    pub fn find_cycles(&self) -> Vec<Vec<usize>> {
+    /// 巡回置換に分解する。
+    /// 計算量: O(NlogN)
+    pub fn into_cycles(self) -> Vec<CyclicPermutation> {
         let mut ab = vec![];
         let mut next = std::collections::HashMap::new();
         let n = self.a.len();
@@ -53,15 +92,35 @@ impl Permutation {
                 }
                 a = b;
             }
-            res.push(chain)
+            res.push(CyclicPermutation::new(chain))
         }
         res
     }
 }
 #[test]
 fn test_cyclic_permutation() {
+    use CyclicPermutation as C;
+
     let p = Permutation::new(vec![0,1,2,3,4], vec![2,0,3,1,4]);
-    assert_eq!(p.find_cycles(), vec![vec![0,2,3,1], vec![4]]);
+    assert_eq!(p.into_cycles(), vec![C::new(vec![0,2,3,1]), C::new(vec![4])]);
+
     let p = Permutation::new(vec![0,1,2,3,4], vec![2,0,1,4,3]);
-    assert_eq!(p.find_cycles(), vec![vec![0,2,1], vec![3,4]]);
+    assert_eq!(p.into_cycles(), vec![C::new(vec![0,2,1]), C::new(vec![3,4])]);
+
+    let p = Permutation::new(vec![0,5,1,6,3,4,2], vec![5,0,6,3,4,1,2]);
+    assert_eq!(p.into_cycles(), vec![C::new(vec![0,5]), C::new(vec![1,6,3,4]), C::new(vec![2])]);
+}
+#[test]
+fn test_transpositions() {
+    let p = Permutation::new(vec![0,5,1,3,2,4], vec![5,1,3,2,4,0]);
+    let mut cs = p.into_cycles();
+    assert_eq!(cs.len(), 1);
+    let c = cs.remove(0);
+    assert_eq!(c.into_transpositions(), vec![
+        Transposition { x: 0, y: 4 },
+        Transposition { x: 0, y: 2 },
+        Transposition { x: 0, y: 3 },
+        Transposition { x: 0, y: 1 },
+        Transposition { x: 0, y: 5 },
+    ]);
 }
